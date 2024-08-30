2fc0a306c230c8ce7276b14aa6b534d546f3ce42
Edmart <edmart666@gmail.com>
2024-08-30 13:11:15 +0300

    Update json 2

--- src/games/even.php
+++ src/games/even.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BrainGames\Cli;
+namespace BrainGames\games\even;
 
 use function cli\line;
 use function cli\prompt;
@@ -8,4 +8,5 @@ use function cli\prompt;
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    print_r(line($name = ''));
+    print_r(line($name = '1'));
+    echo 'Это сообщение из другого файла';
