e90717d0860b186d9a2205f09595be8bbf8ff269
Edmart <edmart666@gmail.com>
2024-08-30 12:31:57 +0300

    com lint

--- src/Cli.php
+++ src/Cli.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace BrainGames\Cli;
 
 use function cli\line;
@@ -7,6 +8,4 @@ use function cli\prompt;
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    print_r(line($name='1')); 
-
-?>
+    print_r(line($name = '1'));
