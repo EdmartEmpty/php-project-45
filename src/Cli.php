6973b204b09979b4383daa21b98ecf8f02c12f3b
Edmart <edmart666@gmail.com>
2024-08-30 11:54:03 +0300

    autoload unclude

--- src/Cli.php
+++ src/Cli.php
@@ -1,6 +1,12 @@
 <?php
 namespace BrainGames\Cli;
 
-use Cli\Collection;
+use function cli\line;
+use function cli\prompt;
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    print_r(line($name='1')); 
 
 ?>
