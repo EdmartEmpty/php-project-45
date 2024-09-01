51224b552ccb8da3be10acee7c943a070b24c619
Edmart <edmart666@gmail.com>
2024-09-01 16:56:37 +0300

    brain-even v.1

--- src/even.php
+++ src/even.php
@@ -1,11 +1,39 @@
 <?php
 
-namespace BrainGames\Cli;
+namespace BrainGames\even;
 
 use function cli\line;
 use function cli\prompt;
 
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    print_r(line($name = ''));
+function getRightAnwer()
+{
+    line('Добрый день!');
+        $name = prompt("Как вас зовут?\n");
+        line("Привет {$name}!");
+        line("Угадайте четное ли число?");
+
+    for ($i = 0; $i < 3; $i++) {
+            $randcount = random_int(1, 10);
+            line("{$randcount}\n");
+            $useranswer = prompt("Ваш ответ\n");
+            $useranswer = strtolower($useranswer);
+            $rightanwem = '';
+        switch (true) {
+            case $randcount % 2 == 0:
+                $rightanwem = 'yes';
+                break;
+            case $randcount % 2 !== 0:
+                $rightanwem = 'no';
+                break;
+            default:
+                throw new \Exception('Нужно использовать только Yes и No');
+        }
+        if ($useranswer === $rightanwem) {
+                echo "Супер!\n";
+        } else {
+                echo "Попробуйте еще раз!\n";
+        }
+    }
+         echo 'Конец игры!';
+}
+     getRightAnwer();
