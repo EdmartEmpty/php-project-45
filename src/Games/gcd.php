cc1e291b0fe72a8fe13766fd7d9f2ce98e0dc043
Edmart <edmart666@gmail.com>
2024-09-16 18:20:45 +0300

    revers

--- src/Games/gcd.php
+++ src/Games/gcd.php
@@ -6,27 +6,20 @@ use function BrainGames\startGame\startGame;
 
 use const BrainGames\startGame\{MAX_RANGE,MIN_RANGE};
 
-// function gcd(mixed $a, mixed $b)
-// {
-//     if ($b != 0) {
-//         return gcd($b, $a % $b);
-//     } else {
-//         return $a;
-//     }
-// }
+function gcd(mixed $a, mixed $b)
+{
+    if ($b != 0) {
+        return gcd($b, $a % $b);
+    } else {
+        return $a;
+    }
+}
 
 
 function getGcdDate()
 {
     $ruls = "Find the greatest common divisor of given numbers.";
-    function gcd(mixed $a, mixed $b)
-    {
-        if ($b != 0) {
-            return gcd($b, $a % $b);
-        } else {
-            return $a;
-        }
-    }
+   
     $gameDate = function () {
         $num1 = random_int(MIN_RANGE, MAX_RANGE);
         $num2 = random_int(MIN_RANGE, MAX_RANGE);
