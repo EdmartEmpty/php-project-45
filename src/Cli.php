3f5ef2e0091295eba5b5f36b5779110c284c17a4
Edmart <edmart666@gmail.com>
2024-09-16 14:41:41 +0300

    update Cli

--- src/Cli.php
+++ src/Cli.php
@@ -1,15 +1,13 @@
 <?php
 
-namespace BrainGame\cli;
+namespace BrainGame\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function hello()
+function getHello()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    exit;
-    return hello();
 }
